89e6833c4cf1787da77880421b4708fbcf184855
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-18 22:45:38 +0530

    error_handling

---
+++ day12/error_handling.py
@@ -0,0 +1,22 @@
+def safe_int_conversion(value):
+    try:
+        return int(value)
+    except ValueError:
+        print("Invalid integer input")
+        return None
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print("File not found")
+        return None
+
+
+if __name__ == "__main__":
+    print(safe_int_conversion("123"))
+    print(safe_int_conversion("abc"))
+
+    print(read_file("sample.txt"))
